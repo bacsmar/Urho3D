3cfabfa8cda1913ac9c53d239e6cf9b1dda41f39
JimMarlowe <jimmarlowe.of.mars1@gmail.com>
2019-04-15 16:42:10 -0700

    Adding AUI+AS launcher program

---
+++ bin/Launcher.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set "dirname=%~dp0"
+if %0 == "%~dpnx0" where /q "%cd%:%~nx0" && set "dirname=%cd%\"
+if exist "%dirname%Urho3DPlayer.exe" (set "DEBUG=") else (set "DEBUG=_d")
+if [%1] == [] (set "OPT1=-w -s") else (set "OPT1=")
+start "" "%dirname%Urho3DPlayer%DEBUG%" Data/AUI/Launcher/Scripts/100_launcher.as %OPT1% %* -p "Data;CoreData;Data/AUI;Data/AUI/Launcher"
